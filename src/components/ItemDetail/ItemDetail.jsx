0b85661922d32d923214cc96995fbfc78ea0f853
Marisol Marecos <marisolmarecos@Marisols-MacBook-Pro.local>
2026-02-28 19:14:53 -0300

    producto agregado al carrito y agotado

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,15 +1,18 @@
 import "./ItemDetail.css";
 import ItemCount from "../ItemCount/ItemCount.jsx";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../../context/CartContext.jsx";
+import { Link } from "react-router";
 
 
 const ItemDetail = ({ product }) => {
     const { addProduct } = useContext(CartContext);
+    const [itemAdded , setItemAdded] = useState(false);
 
     const addToCart = (count) => { 
         const newProduct = {...product, quantity:count};
         addProduct(newProduct);
+        setItemAdded(true);
    };
 
     return (
@@ -18,14 +21,30 @@ const ItemDetail = ({ product }) => {
                 <img className="detail-image" src={product.image} alt={product.name}/>
             </div>
             <div className="detail-info-container">
-                <p className="detail-title">{product.name}</p>
-                <p className="detail-description">{product.description}</p>
-                <p className="detail-price">Precio: ${product.price}</p>        
-                <ItemCount stock={product.stock} addToCart={addToCart} />
+                <div>
+                    <p className="detail-title">{product.name}</p>
+                    <p className="detail-description">{product.description}</p>
+                </div>
+                {
+                    product.stock <= 0 ?  (
+                        <div><p className="detail-stock">Producto agotado</p></div>
+                    ) : (
+                        <><div><p className="detail-price">Precio: ${product.price}</p></div>
+                        {
+                            itemAdded ? (
+                                <div>
+                                <p className="added-message">Producto agregado al carrito</p>
+                                <Link className="link-button" to="/cart">Ir al carrito</Link>
+                                </div>
+                            ) : ( 
+                                <ItemCount stock={product.stock} addToCart={addToCart} />
+                            )
+                        }
+                        </>
+                    )
+                }
             </div>
-            
         </div>
-
     );
 };
 
